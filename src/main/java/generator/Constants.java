2b4ac443e160d9a046c8ba9df54c404c413eae69
ValenButtignol <valentinbuttignol@gmail.com>
2025-02-07 11:44:33 -0300

    Update main

--- src/main/java/generator/Constants.java
+++ src/main/java/generator/Constants.java
@@ -6,4 +6,9 @@ public class Constants {
     public static final String PROPERTIES_CLASS_PATH = "../llm-repok-generator/output/PropertiesClass.java";
     public static final String LLM_TOOL_DIR_PATH = "tools/llm-repok-generator";
     public static final String VERIFIER_TOOL_DIR_PATH = "tools/repok-verifier-and-injector";
+    public static final String GLOBAL_PROMPT_TYPE = "global";
+    public static final String FS_W_PROMPT_TYPE = "fs-w";
+    public static final String FS_P_PROMPT_TYPE = "fs-p";
+    public static final String DUAL_W_PROMPT_TYPE = "dual-w";
+    public static final String DUAL_P_PROMPT_TYPE = "dual-p";
 }
--- src/main/java/generator/Main.java
+++ src/main/java/generator/Main.java
@@ -1,119 +1,53 @@
 package generator;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import generator.executor.LlmExecutor;
+import generator.executor.VerifierAndInjectorExecutor;
+import generator.executor.Executor;
 
 public class Main {
     
-    private static final String PYTHON_PATH = "python3";
-    private static final String SCRIPT_PATH = "tools/llm-repok-generator/main.py";
-    private static final String OUTPUT_TYPE = "console";
-
     public static void main(String[] args) {
 
         String modelName = "Llama3.2";
         String classPath = "LinkedList.java";
         String nameOfClass = "LinkedList";
-        String classString = "public class LinkedList {\n" + //
-                        "\n" + //
-                        "    private static class Node {\n" + //
-                        "\n" + //
-                        "        int data;\n" + //
-                        "\n" + //
-                        "        Node next;\n" + //
-                        "\n" + //
-                        "        Node(int data) {\n" + //
-                        "            this.data = data;\n" + //
-                        "            this.next = null;\n" + //
-                        "        }\n" + //
-                        "    }\n" + //
-                        "\n" + //
-                        "    private Node head;\n" + //
-                        "\n" + //
-                        "    private int size;\n" + //
-                        "\n" + //
-                        "    public LinkedList() {\n" + //
-                        "        head = null;\n" + //
-                        "        size = 0;\n" + //
-                        "    }\n" + //
-                        "\n" + //
-                        "    public void add(int data) {\n" + //
-                        "        Node newNode = new Node(data);\n" + //
-                        "        if (head == null) {\n" + //
-                        "            head = newNode;\n" + //
-                        "        } else {\n" + //
-                        "            Node current = head;\n" + //
-                        "            while (current.next != null) {\n" + //
-                        "                current = current.next;\n" + //
-                        "            }\n" + //
-                        "            current.next = newNode;\n" + //
-                        "        }\n" + //
-                        "        size++;\n" + //
-                        "    }\n" + //
-                        "\n" + //
-                        "    public int remove() {\n" + //
-                        "        if (head == null) {\n" + //
-                        "            throw new IllegalStateException(\"List is empty\");\n" + //
-                        "        }\n" + //
-                        "        int removedData = head.data;\n" + //
-                        "        head = head.next;\n" + //
-                        "        size--;\n" + //
-                        "        return removedData;\n" + //
-                        "    }\n" + //
-                        "}\n" + //
-                        "";
-        String promptType = "fs-w";
+        String promptType = "dual-w";
+        String classString = getClassString(classPath);
+        String specClassPath = getSpecClassPath(promptType);
 
-        String specClassPath = "../llm-repok-generator/output/RepOkClass.java";
+        Executor llmExecutor = new LlmExecutor(modelName, promptType, classString, nameOfClass);
+        Executor verifierExecutor = new VerifierAndInjectorExecutor(classPath, nameOfClass, promptType, specClassPath);
 
-        String[] command = buildGeneratorCommand(modelName, classString, nameOfClass, promptType);
-        command = buildVerifierAndInjectorCommand(classPath, nameOfClass, promptType, specClassPath);
-        
-        execute(command);
-
+        llmExecutor.execute();
+        verifierExecutor.execute();
     }
 
-    public static void execute(String[] command) {
-
+    private static String getClassString(String classPath) {
         try {
-            ProcessBuilder processBuilder = new ProcessBuilder(command);
-            processBuilder.directory(new File("tools/repok-verifier-and-injector"));
-            
-            processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-            process.waitFor();
-            
+            return Files.readString(Path.of(classPath));
         } catch (Exception e) {
-            System.out.println("Error executing command: " + e.getMessage());   
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String getSpecClassPath(String promptType) {
+        switch (promptType) {
+            case Constants.GLOBAL_PROMPT_TYPE:
+                return Constants.REPOK_CLASS_PATH;
+            case Constants.FS_W_PROMPT_TYPE:
+                return Constants.REPOK_CLASS_PATH;
+            case Constants.FS_P_PROMPT_TYPE:
+                return Constants.REPOK_CLASS_PATH;
+            case Constants.DUAL_W_PROMPT_TYPE:
+                return Constants.PROPERTIES_CLASS_PATH;
+            case Constants.DUAL_P_PROMPT_TYPE:
+                return Constants.PROPERTIES_CLASS_PATH;
+            default:
+                return null;
         }
     }
-
-    private static String[] buildGeneratorCommand(String modelName, String classString, String nameOfClass, String promptType) {
-        return new String[] {
-            PYTHON_PATH,
-            SCRIPT_PATH,
-            "-mn", modelName,
-            "-pt", promptType,
-            "-rc", classString,
-            "-cn", nameOfClass,
-            "-ot", OUTPUT_TYPE
-        };
-    }
-
-    // Must execute the verifier and injector with "./repok-verifier-and-injector/gradlew run --args="<classPath> <className> <promptType>""
-    private static String[] buildVerifierAndInjectorCommand(String classPath, String className, String promptType, String specClassPath) {
-        return new String[] {
-            "./gradlew",
-            "run",
-            "--args=" + "../../" + classPath + " " + className + " " + promptType + " " + specClassPath
-        };
-    }
-
 }

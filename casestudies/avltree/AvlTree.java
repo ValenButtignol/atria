f7af5569a6e891227957575bf9d2dded1fb4d5fc
ValenButtignol <valentinbuttignol@gmail.com>
2025-05-01 20:59:19 -0300

    Clean casestudies

--- casestudies/avltree/AvlTree.java
+++ casestudies/avltree/AvlTree.java
@@ -136,4 +136,18 @@ public class AvlTree {
             height = 0;
         }
     }
+
+    public boolean property() {
+        return isBst(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    private boolean isBst(AvlNode node, int min, int max) {
+        if (node == null) {
+            return true;
+        }
+        if (node.element <= min || node.element >= max) {
+            return false;
+        }
+        return isBst(node.left, min, node.element) && isBst(node.right, node.element, max);
+    }
 }
